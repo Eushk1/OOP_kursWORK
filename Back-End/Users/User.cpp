58dc8f0adb34d0313be76d9a1da023038a25d1d0
Eushk1 <151998466+Eushk1@users.noreply.github.com>
2024-12-23 20:46:50 +0300

    Add files via upload

---
+++ Back-End/Users/User.cpp
@@ -0,0 +1,46 @@
+//
+//  User.cpp
+//  KursWork_OOP_Back-end
+//
+//  Created by Егор on 22.12.2024.
+//
+
+#include "User.hpp"
+
+
+User ::User () : user_id(0), last_name(""), first_name(""), username(""), email(""), role(UserRole::Reader) {}
+
+User ::User (int id, const std::string& lastName, const std::string& firstName, const std::string& username, const std::string& email, UserRole role)
+    : user_id(id), last_name(lastName), first_name(firstName), username(username), email(email), role(role) {}
+
+UserRole User::getRole() const {
+    return role;
+}
+
+std::string User::roleToString() const {
+    switch (role) {
+        case UserRole::Admin:
+            return "Admin";
+        case UserRole::Reader:
+            return "Reader";
+        default:
+            return "Unknown";
+    }
+}
+
+std::string User::getLastName() const {
+    return last_name;
+}
+
+std::string User::getFirstName() const {
+    return first_name;
+}
+
+std::string User::getUsername() const {
+    return username;
+}
+
+std::string User::getEmail() const {
+    return email;
+}
+
